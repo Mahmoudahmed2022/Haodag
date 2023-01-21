b0e7f11bbd27053696068e8dba68aace7fe2ca37
kariemAtef <katef4421@gmail.com>
2023-01-22 01:23:40 +0200

    editing on Login2.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Registration from "./components/Registration.js";
-import Login from "./components/Login";
-import Home from "./components/Home.js";
+// import Navbar from "./components/Navbar";
+// import Registration from "./components/Registration.js";
+// import Login from "./components/Login";
+// import Home from "./components/Home.js";
 import Login2 from "./components/Login2";
 function App() {
   return (
