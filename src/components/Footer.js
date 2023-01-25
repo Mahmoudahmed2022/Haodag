c5174eec6cabb2698c7bf8c6a16a7ebef5ba3a48
kariemAtef <katef4421@gmail.com>
2023-01-25 04:03:32 +0200

    icons on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGooglePlus } from "react-icons/fa";
 import image2 from "./images/logo1.png";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 import { Link } from "react-router-dom";
 import "../Css/Footer.css";
@@ -14,18 +16,25 @@ function Footer() {
         <div className="footer">
           <div className="top-footer">
             <div className="top-left-footer">
-              <Link to="/">
-                <FaFacebook className="icon-footer" />
+              <Link to="/" className="aLink color-facebook">
+                <FaFacebook className="icon-footer " />
               </Link>
-              <Link to="/">
+              <Link to="/"className="aLink">
                 <FaGithub className="icon-footer" />
               </Link>
-              <Link to="/">
+              <Link to="/"className="aLink color-linkden">
                 <FaLinkedin className="icon-footer" />
               </Link>
-              <Link to="/">
+              <Link to="/"className="aLink color-google">
                 <FaGooglePlus className="icon-footer" />
               </Link>
+              <Link to="/"className="aLink color-whatsapp">
+                <FaWhatsapp className="icon-footer" />
+              </Link>
+              <Link to="/"className="aLink color-twitter">
+                <FaTwitterSquare className="icon-footer" />
+              </Link>
+              
             </div>
             <div className="top-right-footer">
               <Link className="link-footrer" to="/">
@@ -43,10 +52,11 @@ function Footer() {
             </div>
           </div>
           <div className="bottom-footer">
-            <p className="copyRight">ِAll Rights Reserved To The Team of </p>
-            <Link to="/">
-              <img className="avatar1" src={image2} alt="" />
-            </Link>
+            <p className="copyRight">ِAll Rights Reserved To The Team of <Link to="/" className="avatar1">
+                HAODAG
+              {/* <img className="avatar1" src={image2} alt="" /> */}
+            </Link></p>
+           
           </div>
         </div>
       </div>
