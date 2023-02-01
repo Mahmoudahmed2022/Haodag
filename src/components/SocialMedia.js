08d3c243fdc1e0111e6797d9014b2c398a96abbb
Mahmoudahmed2022 <79380555+Mahmoudahmed2001@users.noreply.github.com>
2023-02-01 20:53:38 +0200

    adding terms of use argument

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,7 +1,29 @@
+import { GoLocation } from "react-icons/go";
+import { HiOutlineMail } from "react-icons/hi";
+import { HiOutlinePhone } from "react-icons/hi";
+import "../Css/SocialMedia.css";
 function SocialMedia() {
   return (
     <>
-      <h2>SocialMedia</h2>
+      <div className="big-SocialMedia-text-container">
+        <div className="header-SocialMedia-text">
+          <h2>Social media</h2>
+        </div>
+        <div className="body-SocialMedia-text">
+          <p>
+            This privacy policy has been developed in order to provide a better
+            service and to take into account the basics and international
+            standards for the use of "personally identifiable information" of
+            Internet users. Personally identifiable information is information
+            that can be used on its own or with other information in order to
+            identify, contact, or locate an individual, or to identify an
+            individual in a general context. Please read our Privacy Policy
+            carefully in order to get a clearer understanding of how we collect,
+            use, protect or otherwise handle your Personally Identifiable Data
+            in connection with our Website.
+          </p>
+        </div>
+      </div>{" "}
     </>
   );
 }
--- src/components/TermofUse.js
+++ src/components/TermofUse.js
@@ -1,12 +1,79 @@
+import "../Css/TermsofUse2.css";
+
 function TermofUse() {
   return (
     <>
-      <div className="big-terms-text-container">
-        <div className="header-terms-text">
+      <div className="big-TermofUse-text-container">
+        <div className="header-TermofUse-text">
           <h2>Terms of use</h2>
         </div>
-        <div className="header-terms-text">
-          <p></p>
+        <div className="body-TermofUse-text">
+          <p>
+            These Terms of Use constitute a legally binding agreement made
+            between you, whether personally or on behalf of an entity (“you”)
+            and [business entity name] (“we,” “us” or “our”), concerning your
+            access to and use of the Haodag.com website as well as any other
+            media form, media channel, mobile website or mobile application
+            related, linked, or otherwise connected thereto ("Haodag"). You
+            agree that by accessing the Site, you have read, understood, and
+            agree to be bound by all of these Terms of Use. If you do not agree
+            with all of these Terms of Use, then you are expressly prohibited
+            from using the Site and you must discontinue use immediately.
+            Supplemental Terms of Use or documents that may be posted on the
+            Site from time to time are hereby expressly incorporated herein by
+            reference. We reserve the right, in our sole discretion, to make
+            changes or modifications to these Terms of Use at any time and for
+            any reason. We will alert you about any changes by updating the
+            “Last updated” date of these Terms of Use, and you waive any right
+            to receive specific notice of each such change. It is your
+            responsibility to periodically review these Terms of Use to stay
+            informed of updates. You will be subject to, and will be deemed to
+            have been made aware of and to have accepted, the changes in any
+            revised Terms of Use by your continued use of the Site after the
+            date such revised Terms of Use are posted. The information provided
+            on the Site is not intended for distribution to or use by any person
+            or entity in any jurisdiction or country where such distribution or
+            use would be contrary to law or regulation or which would subject us
+            to any registration requirement within such jurisdiction or country.
+            Accordingly, those persons who choose to access the Site from other
+            locations do so on their own initiative and are solely responsible
+            for compliance with local laws, if and to the extent local laws are
+            applicable. <h5>INTELLECTUAL PROPERTY</h5> RIGHTS Unless otherwise
+            indicated, the Site is our proprietary property and all source code,
+            databases, functionality, software, website designs, audio, video,
+            text, photographs, and graphics on the Site (collectively, the
+            “Content”) and the trademarks, service marks, and logos contained
+            therein (the “Marks”) are owned or controlled by us or licensed to
+            us, and are protected by copyright and trademark laws and various
+            other intellectual property rights and unfair competition laws of
+            the United States, foreign jurisdictions, and international
+            conventions. The Content and the Marks are provided on the Site “AS
+            IS” for your information and personal use only. Except as expressly
+            provided in these Terms of Use, no part of the Site and no Content
+            or Marks may be copied, reproduced, aggregated, republished,
+            uploaded, posted, publicly displayed, encoded, translated,
+            transmitted, distributed, sold, licensed, or otherwise exploited for
+            any commercial purpose whatsoever, without our express prior written
+            permission. Provided that you are eligible to use the Site, you are
+            granted a limited license to access and use the Site and to download
+            or print a copy of any portion of the Content to which you have
+            properly gained access solely for your personal, non-commercial use.
+            We reserve all rights not expressly granted to you in and to the
+            Site, the Content and the Marks.<h5>USER REPRESENTATIONS</h5> By
+            using the Site, you represent and warrant that: all registration
+            information you submit will be true, accurate, current, and
+            complete. you will maintain the accuracy of such information and
+            promptly update such registration information as necessary. you have
+            the legal capacity and you agree to comply with these Terms of Use.
+            not a minor in the jurisdiction in which you reside , or if a minor,
+            you have received parental permission to use the Site. you will not
+            access the Site through automated or non-human means, whether
+            through a bot, script, or otherwise. you will not use the Site for
+            any illegal or unauthorized purpose. your use of the Site will not
+            violate any applicable law or regulation. If you provide any
+            information that is untrue, inaccurate, not current, or incomplete,
+            we have the right to suspend
+          </p>
         </div>
       </div>
     </>
