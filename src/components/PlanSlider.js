a0b31229324834dffbf98201145bd2953c952a60
Mahmoudahmed2022 <79380555+Mahmoudahmed2001@users.noreply.github.com>
2023-04-04 02:50:24 +0200

    intialize plan details page

---
+++ src/components/PlanSlider.js
@@ -0,0 +1,34 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+function PlanSlider(props) {
+  const photos = props.slidimgs;
+
+  return (
+    <>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
+    </>
+  );
+}
+
+export default PlanSlider;
